d15b2567312d4866ef0cea8a18b842c79b2295c7
shubhamni08 <nikamshubham016@gmail.com>
2025-01-31 12:51:45 +0530

    Payload class added

--- src/test/java/Base/Basics.java
+++ src/test/java/Base/Basics.java
@@ -15,22 +15,7 @@ public class Basics {
                 .log().all()    //to log input
                 .queryParam("key","qaclick123")
                 .header("content-type","application/json")
-                .body("{\n" +
-                        "  \"location\": {\n" +
-                        "    \"lat\": -38.3838,\n" +
-                        "    \"lng\": 34.4242\n" +
-                        "  },\n" +
-                        "  \"accuracy\": 50,\n" +
-                        "  \"name\": \"Rahul Shetty Academy\",\n" +
-                        "  \"phone_number\": \"(+91) 98 239 2387\",\n" +
-                        "  \"address\": \"58 Rasul jiwa comp\",\n" +
-                        "  \"types\": [\n" +
-                        "    \"shoe park\",\n" +
-                        "    \"shop\"\n" +
-                        "  ],\n" +
-                        "  \"website\": \"https://rahulshettyacademy.com\",\n" +
-                        "  \"language\": \"french-IN\"\n" +
-                        "}")
+                .body(PayLoad.addPlace())
                 .when()
                 .post("maps/api/place/add/json")
                 .then()
---
+++ src/test/java/Base/PayLoad.java
@@ -0,0 +1,23 @@
+package Base;
+
+public class PayLoad {
+
+    public static String addPlace(){
+        return "{\n" +
+                "  \"location\": {\n" +
+                "    \"lat\": -38.3838,\n" +
+                "    \"lng\": 34.4242\n" +
+                "  },\n" +
+                "  \"accuracy\": 50,\n" +
+                "  \"name\": \"Rahul Shetty Academy\",\n" +
+                "  \"phone_number\": \"(+91) 98 239 2387\",\n" +
+                "  \"address\": \"58 Rasul jiwa comp\",\n" +
+                "  \"types\": [\n" +
+                "    \"shoe park\",\n" +
+                "    \"shop\"\n" +
+                "  ],\n" +
+                "  \"website\": \"https://rahulshettyacademy.com\",\n" +
+                "  \"language\": \"french-IN\"\n" +
+                "}";
+    }
+}
